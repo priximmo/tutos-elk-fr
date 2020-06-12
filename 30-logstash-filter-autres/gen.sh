3383e28fd4d74351020ede484fb6c8c006a243df
xavki <titi>
2020-06-12 18:34:15 +0200

    update 30

--- 30-logstash-filter-autres/gen.sh
+++ 30-logstash-filter-autres/gen.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-echo "10/06/2020;13;TESTU;BEGIN;start job" >> /tmp/input.log
-sleep 5
-echo "10/06/2020;13;TESTU;PROCESS;collect file" >> /tmp/input.log
-sleep 5
-echo "10/06/2020;13;TESTU;PROCESS;process file" >> /tmp/input.log
-sleep 5
-echo "10/06/2020;13;TESTU;PROCESS;update file" >> /tmp/input.log
-sleep 5
-echo "10/06/2020;13;TESTU;END;end job" >> /tmp/input.log
+echo "127.0.0.1 10/06/2020 13 TESTU;BEGIN;start job-0.1;10156" | tee -a /tmp/input.log
+sleep 3
+echo "127.0.0.2 10/06/2020 13 TESTU;PROCESS;start job-0.1;10156" | tee -a /tmp/input.log
+sleep 3
+echo "10.127.0.156 10/06/2020 13 TESTU;BEGIN;start job-0.1;10156" | tee -a /tmp/input.log
+sleep 3
+
